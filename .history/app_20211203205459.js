4a5e9fac068f75f4196074316341f7cb9488f425
in-woong <78221769+in-woong@users.noreply.github.com>
2021-12-03 20:56:09 +0900

    초기 app file 설정

---
+++ .history/app_20211203205459.js
@@ -0,0 +1,24 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+
+import tweetRouter from "./router/tweetRouter.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan());
+app.use(helmet());
+app.use(cors());
+
+app.use("/tweets", tweetRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "I can't find anything" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({message:"There are something wrong"});
+});
