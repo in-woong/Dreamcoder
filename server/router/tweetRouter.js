e4052f2265d5010e9a4c20feb1a8d9cf7a65472c
in-woong <78221769+in-woong@users.noreply.github.com>
2021-12-18 23:07:46 +0900

    수정 / Auth middleware 최종 수정

--- server/router/tweetRouter.js
+++ server/router/tweetRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {body} from "express-validator";
+import { body } from "express-validator";
 import "express-async-errors";
 import * as tweetController from "../controller/tweet.js";
 import validate from "../middleware/validator.js";
+import isAuth from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -18,10 +19,10 @@ router.get("/", tweetController.getTweets); //getTweets
 
 router.get("/:id", tweetController.getTweet); //getTweet
 
-router.post("/", validateTweet, tweetController.createTweet); //createTweet
+router.post("/", validateTweet, isAuth, tweetController.createTweet); //createTweet
 
-router.put("/:id",validateTweet, tweetController.updateTweet); //updateTweet
+router.put("/:id", validateTweet, isAuth, tweetController.updateTweet); //updateTweet
 
-router.delete("/:id", tweetController.deleteTweet);
+router.delete("/:id", isAuth, tweetController.deleteTweet);
 
 export default router;
