d238b4e772a3298875cda9eac8feb5e87c846a26
in-woong <78221769+in-woong@users.noreply.github.com>
2021-12-18 17:29:22 +0900

    추가 / jwt, bcrypt 사용해서 구현

--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -1,16 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
+
+const hashNum = 10;
 
 let users = [
   {
     username: "ellie",
-    password: "12345",
+    password: "$2b$10$4HRXP76rkUqDOY9/bUa0duaeI8BebbuhV.BSuO00Rn3ONAcXi6UD.",
     name: "Ellie",
     email: "ellie@gmail.com",
     url: "https://widgetwhats.com/app/uploads/2019//11/free-profile-photo-whatsapp-1.png",
   },
 ];
+
+const secret = "secret";
 
 router.post("/signup", (req, res, next) => {
   const { username, password, name, email, url } = req.body;
@@ -18,25 +24,49 @@ router.post("/signup", (req, res, next) => {
     console.log("SomeOne already have this Username");
     return res.sendStatus(404);
   }
-  const newUser = { username, password, name, email, url };
+
+  const hashed = bcrypt.hashSync(password, hashNum);
+
+  const newUser = { username, password: hashed, name, email, url };
   users.push(newUser);
-  res.status(200).json(users);
+  const token = jwt.sign(
+    {
+      id: username,
+      isAdmin: false,
+    },
+    secret
+  );
+  res.status(200).json({ username, token });
 });
 
 router.post("/login", (req, res, next) => {
   const { username, password } = req.body;
   const user = users.find((user) => user.username == username);
-  console.log("fuck");
-  if (user.password == password) {
-    return res
-      .status(200)
-      .json({ username: user.username, password: user.password });
+  //login을 하면 id, isAdmin을 payload, 임의의 secret을 가지고, option을 주면서 token 생성
+  const result = bcrypt.compareSync(password, user.password);
+  if (!result) {
+    return res.sendStatus(404);
   }
+  const token = jwt.sign(
+    {
+      id: username,
+      isAdmin: false,
+    },
+    secret
+  );
+
+  return res.status(200).json({ username: user.username, token });
 });
 
-router.get("/me", (req,res,next)=>{
-    //내 토큰을 보내서, 토큰이 유효한지 확인
-    res.sendStatus(200);
+router.get("/me", (req, res, next) => {
+  const { username, token } = req.body;
+
+  jwt.verify(token, secret, (error, decoded) => {
+    if (error) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send(decoded);
+  });
 });
 
 export default router;
