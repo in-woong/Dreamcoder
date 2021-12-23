1f8fa768d78b33d8cd5690561095b18bb3b4d759
in-woong <78221769+in-woong@users.noreply.github.com>
2021-12-23 11:43:24 +0900

    REFACTOR about configuration

--- server/config.js
+++ server/config.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+}
+
 export const config = {
   jwt: {
-    secretKey: process.env.JWT_SECRET,
-    expiresInSec: process.env.JWT_EXPIRES_SEC,
+    secretKey: required("JWT_SECRET"),
+    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
   },
   bcrypt: {
-    saltRounds: process.env.BCRYPT_SALT_ROUNDS,
+    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
+  },
+  host: {
+    port: parseInt(required("HOST_PORT", 8080)),
   },
 };
