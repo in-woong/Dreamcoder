ce9a6397098af8fd6d5cb46cd3c1ddb7d82b3413
in-woong <78221769+in-woong@users.noreply.github.com>
2022-01-02 16:25:17 +0900

    REFACTOR / youtube service

--- Projects/youtube_ellie/src/app.jsx
+++ Projects/youtube_ellie/src/app.jsx
@@ -1,45 +1,24 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
+
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/vide_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&part=snippet &maxResults=25&key=AIzaSyCVxtND4l6jCAPpIj1dkCykF31V-ObiiJ0`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((results) => setVideos(results))
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCVxtND4l6jCAPpIj1dkCykF31V-ObiiJ0',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
-  // []는 마운트 되었을때만 호출
-  //  공란은 state나 prop이 업데이트 될때마다 호출
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
---
+++ Projects/youtube_ellie/src/service/youtube.js
@@ -0,0 +1,31 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+
+  mostPopular() {
+    return fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+      this.getRequestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => result.items);
+  }
+
+  search(query) {
+    return fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&part=snippet &maxResults=25&key=${this.key}`,
+      this.getRequestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      );
+  }
+}
+
+export default Youtube;
