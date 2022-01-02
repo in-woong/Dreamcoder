410ef3099e533fc6623d96d0c8833007567cd4b5
in-woong <78221769+in-woong@users.noreply.github.com>
2022-01-02 16:01:02 +0900

    CREATE / make search function

--- Projects/youtube_ellie/src/app.jsx
+++ Projects/youtube_ellie/src/app.jsx
@@ -1,24 +1,51 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/vide_list/video_list';
 
 function App() {
-  const [videos, setVideos] =useState([]);
+  const [videos, setVideos] = useState([]);
 
-  useEffect(()=>{
+  const search = (query) => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&part=snippet &maxResults=25&key=AIzaSyCVxtND4l6jCAPpIj1dkCykF31V-ObiiJ0`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((results) => setVideos(results))
+      .catch((error) => console.log('error', error));
+  };
+
+  useEffect(() => {
     const requestOptions = {
       method: 'GET',
-      redirect: 'follow'
+      redirect: 'follow',
     };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCVxtND4l6jCAPpIj1dkCykF31V-ObiiJ0", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  },[])
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCVxtND4l6jCAPpIj1dkCykF31V-ObiiJ0',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
   // []는 마운트 되었을때만 호출
   //  공란은 state나 prop이 업데이트 될때마다 호출
-  return <VideoList videos={videos} />
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ Projects/youtube_ellie/src/components/search_header/search_header.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+const SearchHeader = ({onSearch}) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+    inputRef.current.value ="";
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src='/images/logo.png' alt='logo' />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        className={styles.input}
+        ref={inputRef}
+        type='search'
+        placeholder='Search...'
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type='submit' onClick={onClick}>
+        <img className={styles.buttonImg} src='/images/search.png' alt='' />
+      </button>
+    </header>
+  );
+};
+
+export default SearchHeader;
