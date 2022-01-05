ec3d990b5d8609f489633769891653dcaa6d2fd7
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-01-06 00:30:21 +0900

    CREATE / card-maker login css

--- Projects/card-maker/src/app.jsx
+++ Projects/card-maker/src/app.jsx
@@ -1,7 +1,29 @@
-import './app.css';
+import styles from "./app.module.css";
+
 
 function App() {
-  return <h1>Hello :)</h1>
+  return (
+    <main>
+      <header className={styles.header}>
+        <img className={styles.header_img} src={require("./favicon.jpg")} alt="/" />
+        <span className={styles.header_span}>Bussiniss Card Maker</span>
+      </header>
+      <section className={styles.login}>
+        <span className={styles.login_span}>Login</span>
+        <div className={styles.login_SSO}>
+          <div className={styles.login_google}>
+            Google
+          </div>
+          <div className={styles.login_github}>
+            Github
+          </div>
+        </div>
+      </section>
+      <footer className={styles.footer}>
+        <span className={styles.footer_span}>Create your dream</span>
+      </footer>
+    </main>
+  )
 }
 
 export default App;
