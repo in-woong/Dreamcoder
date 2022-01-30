39d7ee87e0de9284985c56fa383c1d8c5eb3d267
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-01-30 15:43:55 +0900

    복습 4

--- Projects/card_maker_re/src/simpleHabit.jsx
+++ Projects/card_maker_re/src/simpleHabit.jsx
@@ -24,3 +24,4 @@ const SimpleHabit = (props) => {
 export default SimpleHabit;
 
 //함수형은 class 형과 다르게 render가 아니라 지역함수도 계속 해서 rendering 및 비즈니스 로직들이 수행이된다.
+//따라서 함수를 새로 설명하지 않고 동일하게 전달하기 위해 useCallback 을 사용할 수 있다
